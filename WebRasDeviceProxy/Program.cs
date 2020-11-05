2dbc3ce2dfe02bba9c4f5d18c9ae6d4737dffff8
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-11-05 06:00:29 +0100

    read connection string parameters from environment

--- WebRasDeviceProxy/Program.cs
+++ WebRasDeviceProxy/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
 
 namespace WebRasDeviceProxy
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to WebRas Device Proxy");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables("WebRas_")
+                .Build();
+
+            var connectionString = BuildConnectionString(configuration);
+
+            using (var deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt))
+            {
+                deviceClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes("Successfully connected to IoT Hub")));
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string BuildConnectionString(IConfiguration configuration)
+        {
+            var hostName = configuration["HostName"];
+            var deviceId = configuration["DeviceId"];
+            var key = configuration["Key"];
+
+            var connectionString =
+                $"HostName={hostName};DeviceId={deviceId};SharedAccessKey={key}";
+            return connectionString;
         }
     }
 }
